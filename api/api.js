d846660551004396662b8360aea6f5ef0b0a8919
Mike Conley <mike.d.conley@gmail.com>
2019-08-21 10:54:44 -0400

    Make work after bug 1451212.

--- api/api.js
+++ api/api.js
@@ -20,69 +20,65 @@ this.reflows = class extends ExtensionAPI {
 
     return {
       reflows: {
-        onUninterruptableReflow: new EventManager(context, "reflow-api", fire => {
+        onUninterruptableReflow: new EventManager({
+          context,
+          name: "reflow-api",
+          register: fire => {
+            const windowTracker = ExtensionParent.apiManager.global.windowTracker;
+            let windows = Array.from(windowManager.getAll(), win => win.window);
 
-          const windowTracker = ExtensionParent.apiManager.global.windowTracker;
-          let windows = Array.from(windowManager.getAll(), win => win.window);
-
-          let observeWindow = (win) => {
-            let observer = {
-              reflow(start, end) {
-                // Grab the stack, but slice off the top frame inside this observer.
-                let stack = new Error().stack.split("\n").slice(1).join("\n");
-                // If the stack string is empty, and a debugger is attached, try to
-                // hit a breakpoint.
-                if (!stack) {
-                  let debug = Cc["@mozilla.org/xpcom/debug;1"].getService(Ci.nsIDebug2);
-                  if (debug.isDebuggerAttached) {
-                    debug.break("api.js", -1);
+            let observeWindow = (win) => {
+              let observer = {
+                reflow(start, end) {
+                  // Grab the stack, but slice off the top frame inside this observer.
+                  let stack = new Error().stack.split("\n").slice(1).join("\n");
+                  // If the stack string is empty, and a debugger is attached, try to
+                  // hit a breakpoint.
+                  if (!stack) {
+                    let debug = Cc["@mozilla.org/xpcom/debug;1"].getService(Ci.nsIDebug2);
+                    if (debug.isDebuggerAttached) {
+                      debug.break("api.js", -1);
+                    }
                   }
-                }
 
-                let id = windowTracker.getId(win);
-                fire.async(id, start, end, stack);
-              },
-              reflowInterruptible(start, end) {},
-              QueryInterface: ChromeUtils.generateQI([Ci.nsIReflowObserver,
-                                                      Ci.nsISupportsWeakReference])
+                  let id = windowTracker.getId(win);
+                  fire.async(id, start, end, stack);
+                },
+                reflowInterruptible(start, end) {},
+                QueryInterface: ChromeUtils.generateQI([Ci.nsIReflowObserver,
+                                                        Ci.nsISupportsWeakReference])
+              };
+
+              win.docShell.addWeakReflowObserver(observer);
+
+              windowMap.set(win, observer);
             };
 
-            let docShell = win.QueryInterface(Ci.nsIInterfaceRequestor)
-                              .getInterface(Ci.nsIWebNavigation)
-                              .QueryInterface(Ci.nsIDocShell);
-            docShell.addWeakReflowObserver(observer);
-
-            windowMap.set(win, observer);
-          };
-
-          for (let win of windows) {
-            observeWindow(win);
-          }
-
-          let windowOpenListener = (win) => {
-            observeWindow(win);
-          }
-
-          windowTracker.addListener("domwindowopened", windowOpenListener);
-          windows = [];
-
-          return () => {
-            let windows = Array.from(windowManager.getAll(), win => win.window);
             for (let win of windows) {
-              let observer = windowMap.get(win);
-              if (observer) {
-                let docShell = win.QueryInterface(Ci.nsIInterfaceRequestor)
-                                  .getInterface(Ci.nsIWebNavigation)
-                                  .QueryInterface(Ci.nsIDocShell);
-                docShell.removeWeakReflowObserver(observer);
-              }
-
-              windowMap.delete(win);
+              observeWindow(win);
             }
 
-            windowTracker.removeListener("domwindowopened", windowOpenListener);
-          }
+            let windowOpenListener = (win) => {
+              observeWindow(win);
+            }
 
+            windowTracker.addListener("domwindowopened", windowOpenListener);
+            windows = [];
+
+            return () => {
+              let windows = Array.from(windowManager.getAll(), win => win.window);
+              for (let win of windows) {
+                let observer = windowMap.get(win);
+                if (observer) {
+                  win.docShell.removeWeakReflowObserver(observer);
+                }
+
+                windowMap.delete(win);
+              }
+
+              windowTracker.removeListener("domwindowopened", windowOpenListener);
+            }
+          }
         }).api()
       }
     };
